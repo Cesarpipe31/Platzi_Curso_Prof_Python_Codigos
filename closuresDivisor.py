ea7c471ec7f319ed22f50f230d075dd6e952705b
Cesarpipe31 <cesarpipe31ingenieria@gmail.com>
2022-10-04 14:34:30 -0500

    Avances

---
+++ closuresDivisor.py
@@ -0,0 +1,21 @@
+
+# This closure returns a function that returns the 
+# division of an x number by n
+
+def make_division_by(n):
+    def repeater(number):
+        return number / n
+    return repeater
+
+def run():
+    division_by_3 = make_division_by(3)
+    print(division_by_3(18))
+    division_by_5 = make_division_by(5)
+    print(division_by_5(100))
+    division_by_18 = make_division_by(18)
+    print(division_by_18(54))
+
+
+if __name__ == "__main__":
+    run()
+
